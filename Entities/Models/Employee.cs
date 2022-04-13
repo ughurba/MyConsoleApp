b1656fa8113f9d75bdeabf6b5de97c09f1fa0b67
ughurba <ughurba@edu.code.az>
2022-04-14 01:47:37 +0400

    create Entities

---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Employee : IEntity
+    {
+        public int Id { get ; set ; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public string RestaurantName { get; set; }
+    }
+}
---
+++ Entities/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Restaurant : IEntity
+    {
+        public int Id { get ; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
