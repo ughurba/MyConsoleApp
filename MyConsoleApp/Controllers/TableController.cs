ebcf028277c37de376803c5fdfd9f1ad9d5f21ae
ughurba <ughurba@edu.code.az>
2022-04-16 17:17:45 +0400

    I wrote everything to the table controller

--- MyConsoleApp/Controllers/TableController.cs
+++ MyConsoleApp/Controllers/TableController.cs
@@ -61,5 +61,96 @@ namespace MyConsoleApp.Controllers
 
 
         }
+
+        public void Delete()
+        {
+            try
+            {
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik id qeyd edin");
+                int id = int.Parse(Console.ReadLine());
+                serviceTable.Delete(id);
+            }catch(Exception ex)
+            {
+                PrintAndEnum.Print(ConsoleColor.Yellow, ex.Message);
+            }
+      
+        }
+        public void GetAll()
+        {
+            foreach (var item in serviceTable.GetAll())
+            {
+                PrintAndEnum.Print(ConsoleColor.Green, $"Id:{item.Id}\nMoney Table:{item.MoneyTabel}\n" +
+                                                 $"Reservition:{item.Reservition}\n");
+
+            }
+            
+        }
+        public  void GetAllTableByRestName()
+        {
+            try
+            {
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik Restaurant Name qeyd edin");
+                string restName = Console.ReadLine();
+
+                foreach (var item in serviceTable.GetAllTableByRestName(restName))
+                {
+                    PrintAndEnum.Print(ConsoleColor.Green, $"Restaurant Name:{item.RestaurantNameTable}\nId:{item.Id}\nMoney Table:{item.MoneyTabel}\n" +
+                                                $"Reservition:{item.Reservition}\n");
+                                              
+                }
+                
+
+            }catch (Exception ex)
+            {
+                PrintAndEnum.Print(ConsoleColor.Red, ex.Message);
+            }
+          
+
+        }
+        public void GetTable()
+        {
+            try
+            {
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik id qeyd edin");
+                int id = int.Parse(Console.ReadLine());
+                PrintAndEnum.Print(ConsoleColor.Green, serviceTable.GetTable(id).TableNo);
+            }catch (Exception ex)
+            {
+                PrintAndEnum.Print(ConsoleColor.Green, ex.Message);
+            }
+          
+           
+        }
+        public void UpdateMoneyTable()
+        {
+            try
+            {
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik id qeyd edin");
+                int id = int.Parse(Console.ReadLine());
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik money qeyd edin");
+                int money = int.Parse(Console.ReadLine());
+                serviceTable.UpdateMoneyTable(id, money);
+            }catch(Exception ex)
+            {
+                PrintAndEnum.Print(ConsoleColor.Green, ex.Message);
+            }
+          
+
+        }
+        public void UpdateTableNo()
+        {
+            try
+            {
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik id qeyd edin");
+                int id = int.Parse(Console.ReadLine());
+                PrintAndEnum.Print(ConsoleColor.Yellow, "Xaiw edirik new Table No qeyd edin");
+                string newTableNo = Console.ReadLine();
+                serviceTable.UpdateTableNo(id, newTableNo);
+            }catch(Exception ex)
+            {
+                PrintAndEnum.Print(ConsoleColor.Green, ex.Message);
+            }
+       
+        }
     }
 }
--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,25 +1,16 @@
-﻿using Business.Service;
-using Entities.Models;
-using MyConsoleApp.Controllers;
+﻿using MyConsoleApp.Controllers;
 using System;
-using Utilities.Extension;
 using Utilities.Helper;
 
 namespace MyConsoleApp
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
-
+            TableController tableController = new TableController();
             RestaurantController restaurantController = new RestaurantController();
-            //ServiceClient serviceClient = new ServiceClient();
-            //ServiceTable serviceTable = new ServiceTable();
-            //ServiceEmployee serviceEmployee = new ServiceEmployee();
-            //ServiceRestaurant serviceRestaurant = new ServiceRestaurant();
-            //RestaurantController restaurantController = new RestaurantController();
-
 
             PrintAndEnum.Print(ConsoleColor.Green, "WELCOME TO OUR RESTAURANT");
 
@@ -27,7 +18,7 @@ namespace MyConsoleApp
             CreateTable();
             CreateEmployee();
             CreateClient();
-
+        EnterName:
             PrintAndEnum.Print(ConsoleColor.Blue, "1-Menu Restaurant\n2-Menu Table\n" +
                 "3-Menu Employee\n4-Menu Client");
             int num = int.Parse(Console.ReadLine());
@@ -37,11 +28,10 @@ namespace MyConsoleApp
                 {
                     case 1:
                         PrintAndEnum.Print(ConsoleColor.Blue, "1-Create Restaurant\n2-Update Restauran by Name\n" +
-                        "3-Update Adress\n4-Delete\n5-Get All\n6-Get Restaurant by Id\n7-Get Restaurant by Name\n");
+                        "3-Update Adress\n4-Delete\n5-Get All\n6-Get Restaurant by Id\n7-Get Restaurant by Name\n0-Main menu");
                         int chocie = int.Parse(Console.ReadLine());
                         switch (chocie)
                         {
-
                             case 1:
                                 restaurantController.Creat();
                                 break;
@@ -63,33 +53,43 @@ namespace MyConsoleApp
                             case 7:
                                 restaurantController.GetRestaurantByName();
                                 break;
-
-
+                            case 0:
+                                goto EnterName;
                         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                         break;
                     case 2:
+                        PrintAndEnum.Print(ConsoleColor.Blue, "1-Create Tabel\n2-Delete Table\n" +
+                        "3-Get All\n4-Get All Table By Restaurant Name\n5-Get Table\n6-Update Money Table\n7-Update Table No\n0-Main menu");
+                        int chocie2 = int.Parse(Console.ReadLine());
+                        switch (chocie2)
+                        {
 
+                            case 1:
+                                tableController.Creat();
+                                break;
+                            case 2:
+                                tableController.Delete();
+                                break;
+                            case 3:
+                                tableController.GetAll();
+                                break;
+                            case 4:
+                                tableController.GetAllTableByRestName();
+                                break;
+                            case 5:
+                                tableController.GetTable();
+                                break;
+                            case 6:
+                                tableController.UpdateMoneyTable();
+                                break;
+                            case 7:
+                                tableController.UpdateTableNo();
+                                break;
+                            case 0:
+                                goto EnterName;
 
-
-
+                        }
                         break;
-
-
                     case 3:
 
 
@@ -116,16 +116,16 @@ namespace MyConsoleApp
         }
         public static void CreateTable()
         {
-           
-                TableController tableController = new TableController();
-                int num;
+
+            TableController tableController = new TableController();
+            int num;
             while (true)
             {
                 try
                 {
                     PrintAndEnum.Print(ConsoleColor.Blue, "1-Creat Table");
                     num = int.Parse(Console.ReadLine());
-               
+
                     if (num == 1)
                     {
                         tableController.Creat();
@@ -141,14 +141,14 @@ namespace MyConsoleApp
         public static void CreateRestaurant()
         {
             RestaurantController restaurantController = new RestaurantController();
-            
+
             while (true)
             {
                 try
                 {
                     PrintAndEnum.Print(ConsoleColor.Blue, "1-Creat Restaurant");
-                    int  num = int.Parse(Console.ReadLine()) ;
-               
+                    int num = int.Parse(Console.ReadLine());
+
                     if (num == 1)
                     {
                         restaurantController.Creat();
